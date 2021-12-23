8087409a04c09163a67ccac1c0a063f33495d2c8
mohamedallam13 <mohamedallam.tu@gmail.com>
2021-12-24 00:34:24 +0100

    Add Tag routes (get, post)

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,13 +17,29 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(id);
+    if (!tagData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
